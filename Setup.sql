3c0a84ab85db98bc44e3f7a383542dfac88772d8
AlejandrOcLz <alejo51515@gmail.com>
2023-03-05 00:47:54 -0500

    setup

---
+++ Setup.sql
@@ -0,0 +1,8 @@
+@D:\Escritorio\TrabajosU\Oracle\TallerCreacionBaseDeDatos\Usere.sql
+@D:\Escritorio\TrabajosU\Oracle\TallerCreacionBaseDeDatos\States.sql
+@D:\Escritorio\TrabajosU\Oracle\TallerCreacionBaseDeDatos\City.sql
+@D:\Escritorio\TrabajosU\Oracle\TallerCreacionBaseDeDatos\Place.sql
+@D:\Escritorio\TrabajosU\Oracle\TallerCreacionBaseDeDatos\Review.sql
+@D:\Escritorio\TrabajosU\Oracle\TallerCreacionBaseDeDatos\Amenity.sql
+@D:\Escritorio\TrabajosU\Oracle\TallerCreacionBaseDeDatos\PlaceAmenity.sql
+
